862b7ccf257a40551ad29736030b7790164cca75
Kristian Ivanov <kriskopk@abv.bg>
2021-02-22 12:53:08 +0200

    Stacks and Queses

---
+++ 01_StacksAndQueues/Lab/1.Reverse-Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> reversedString = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversedString.Push(input[i]);
+            }
+
+            while (reversedString.Count>0)
+            {
+                Console.Write(reversedString.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
