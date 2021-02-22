663ecfcad7ae5b704327c08d0e12e0f720b0f864
Kristian Ivanov <kriskopk@abv.bg>
2021-02-22 13:00:45 +0200

    Matching Brackets

---
+++ 01_StacksAndQueues/Lab/4.Matching-Brackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<int> bracketIndex = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    bracketIndex.Push(i);
+                }
+
+                if (input[i]==')')
+                {
+                    var startIndex = bracketIndex.Pop();
+                    Console.WriteLine(input.Substring(startIndex,i-startIndex+1));
+                }
+            }
+        }
+    }
+}
