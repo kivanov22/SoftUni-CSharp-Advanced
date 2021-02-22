eab0e643ab69dc10fcf2f1072853c79b95da8b08
Kristian Ivanov <kriskopk@abv.bg>
2021-02-22 12:59:48 +0200

    Simple Calculator

---
+++ 01_StacksAndQueues/Lab/3.Simple-Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                stack.Push(input[i]);
+            }
+
+            while (stack.Count>1)
+            {
+                int leftNum = int.Parse(stack.Pop());
+                string sign = stack.Pop();
+                int rightNum = int.Parse(stack.Pop());
+
+                if (sign =="+")
+                {
+                    stack.Push((leftNum + rightNum).ToString());
+                }
+                else if (sign=="-")
+                {
+                    stack.Push((leftNum - rightNum).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
