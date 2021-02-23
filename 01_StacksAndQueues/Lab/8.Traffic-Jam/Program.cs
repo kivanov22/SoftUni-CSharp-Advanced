cf2d01f4650b0caf0cab9773d50c17cdf55d66a5
Kristian Ivanov <kriskopk@abv.bg>
2021-02-23 19:22:27 +0200

    Traffic Jam

---
+++ 01_StacksAndQueues/Lab/8.Traffic-Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsGreen = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            Queue<string> cars = new Queue<string>();
+
+            int countCars = 0;
+
+            while (command!="end")
+            {
+                if (command=="green")
+                {
+                    for (int i = 0; i < carsGreen; i++)
+                    {
+                        if (cars.Count>0)
+                        {
+                            Console.WriteLine(cars.Dequeue() + " passed!");
+                            countCars++;
+                        }
+                    }
+                }
+                else 
+                {
+                    cars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{countCars} cars passed the crossroads.");
+        }
+    }
+}
