c147b5fd4a906a8ed221a38b72ceb962414dd83e
Kristian Ivanov <kriskopk@abv.bg>
2021-02-23 19:21:32 +0200

    Hot Potato

---
+++ 01_StacksAndQueues/Lab/7.Hot-Potato/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> potatoQue = new Queue<string>(children);
+
+            int potatoTosses = 0;
+            while (potatoQue.Count>1)
+            {
+                potatoTosses++;
+                string kid = potatoQue.Dequeue();//remove kid 
+
+                if (potatoTosses==n)
+                {
+                    potatoTosses = 0;//we set toss to 0 
+                    Console.WriteLine("Removed " + kid);
+                }
+                else
+                {
+                    potatoQue.Enqueue(kid);//add kid to first
+                }
+            }
+            Console.WriteLine("Last is " + potatoQue.Dequeue());
+        }
+    }
+}
