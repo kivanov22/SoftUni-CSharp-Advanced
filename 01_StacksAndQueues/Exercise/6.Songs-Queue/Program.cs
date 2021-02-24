a29fcbaa9b3250a14e395ce89df188695394c52f
Kristian Ivanov <kriskopk@abv.bg>
2021-02-24 19:18:25 +0200

    Songs Queue

---
+++ 01_StacksAndQueues/Exercise/6.Songs-Queue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+
+            
+            Queue<string> playlist = new Queue<string>(songs);
+
+            while (playlist.Any())
+            {
+                var commands = Console.ReadLine();
+                
+                if (commands=="Play")
+                {
+                    playlist.Dequeue();
+                }
+                else if (commands.StartsWith("Add"))
+                {
+                    var songName = commands.Substring(4);
+
+                    if (!playlist.Contains(songName))
+                    {
+                        playlist.Enqueue(songName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                }
+                else if (commands=="Show")
+                {
+                    Console.WriteLine(string.Join(", ",playlist));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
