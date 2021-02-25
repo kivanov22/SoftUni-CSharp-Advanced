7f5b323f650618613ea5440c3a2445303f078a20
Kristian Ivanov <kriskopk@abv.bg>
2021-02-25 23:46:15 +0200

    Symbol Matrix

---
+++ 02_MultidimensionalArrays/Lab/04.Symbol-In-Matrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04.Symbol_In_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] data = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+            char symbol =char.Parse(Console.ReadLine());
+            bool isFound = false;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row,col]==symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
