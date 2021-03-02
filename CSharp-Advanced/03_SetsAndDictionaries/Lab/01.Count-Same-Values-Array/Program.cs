d723a4ba43a6b63803f8160456d4b09855d1e3a1
Kristian Ivanov <kriskopk@abv.bg>
2021-03-02 21:18:14 +0200

    Count Same Values Array

---
+++ CSharp-Advanced/03_SetsAndDictionaries/Lab/01.Count-Same-Values-Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Count_Same_Values_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> checkNumbers = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!checkNumbers.ContainsKey(numbers[i]))
+                {
+                    checkNumbers.Add(numbers[i], 0);
+                }
+                checkNumbers[numbers[i]]++;
+            }
+
+            foreach (var time in checkNumbers)
+            {
+                Console.WriteLine($"{time.Key} - {time.Value} times");
+            }
+        }
+    }
+}
