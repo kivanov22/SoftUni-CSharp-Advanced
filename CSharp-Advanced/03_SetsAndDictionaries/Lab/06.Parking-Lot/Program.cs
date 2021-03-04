847b019965a037ba3a5c875e203861193c71a36d
Kristian Ivanov <kriskopk@abv.bg>
2021-03-04 10:25:52 +0200

    Parking Lot

---
+++ CSharp-Advanced/03_SetsAndDictionaries/Lab/06.Parking-Lot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> record = new HashSet<string>();
+
+            while (input!="END")
+            {
+                string[] cmdArgs = input.Split(", ");
+                string command = cmdArgs[0];
+                string plate = cmdArgs[1];
+
+                if (command=="IN")
+                {
+                    record.Add(plate);
+                }
+                else if (command=="OUT")
+                {
+                    record.Remove(plate);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (record.Count>0)
+            {
+                foreach (var car in record)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+ 
+        }
+    }
+}
