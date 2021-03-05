eaf9e64adf2df0a1b2e4cad859257b4dd4ecc23e
Kristian Ivanov <kriskopk@abv.bg>
2021-03-05 17:34:19 +0200

    Sets of Elements

---
+++ CSharp-Advanced/03_SetsAndDictionaries/Exercise/02.Sets-Of-Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+
+            int n = elements[0];
+            int m = elements[1];
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                firstSet.Add(num);
+
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int num2 = int.Parse(Console.ReadLine());
+
+                secondSet.Add(num2);
+            }
+
+            foreach (var item in firstSet)
+            {
+                if (secondSet.Contains(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
