5851e4f768fad147305c3c970b420e41de8465ba
Kristian Ivanov <kriskopk@abv.bg>
2021-03-05 17:35:54 +0200

    Count Symbols

---
+++ CSharp-Advanced/03_SetsAndDictionaries/Exercise/05.Count-Symbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            string input = Console.ReadLine();
+
+           // char[] sort = input.ToCharArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if (!symbols.ContainsKey(current))
+                {
+                    symbols.Add(current, 1);
+
+                }
+                else
+                {
+                    symbols[current]++;
+                }
+            }
+
+            foreach (var item in symbols)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
