de701f3a3e5503ee45ea24cf901e02ed97220d8c
Kristian Ivanov <kriskopk@abv.bg>
2021-03-05 17:33:41 +0200

    Unique Usernames

---
+++ CSharp-Advanced/03_SetsAndDictionaries/Exercise/01-Unique-Usernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> record = new HashSet<string>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string names = Console.ReadLine();
+
+
+                record.Add(names);
+            }
+
+            foreach (var name in record)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
