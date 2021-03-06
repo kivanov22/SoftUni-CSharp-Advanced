5b5a70e93a3422617e6545a37f9b8e5a3827cdbd
Kristian Ivanov <kriskopk@abv.bg>
2021-03-06 17:57:20 +0200

    Slice A File

---
+++ CSharp-Advanced/04_Streams,FilesAndDirectories/Lab/05.Slice-A-File/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace _05.Slice_A_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(FileStream streamReader = new FileStream("sliceMe.txt",FileMode.Open))
+            {
+                int chunkSize = (int)streamReader.Length / 4;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    byte[] buffer = new byte[1];
+                    int count = 0;
+
+                    using(FileStream streamWrite = new FileStream($"slice-{i + 1}.txt", FileMode.Create, FileAccess.Write))
+                    {
+                        while (count<chunkSize)
+                        {
+                            streamReader.Read(buffer, 0, buffer.Length);
+                            streamWrite.Write(buffer, 0, buffer.Length);
+                            count += buffer.Length;
+                        }
+
+                        if (streamReader.Position!=streamReader.Length && i==3)
+                        {
+                            int remainingBytes = (int)streamReader.Length - (int)streamReader.Position;
+                            byte[] lastBuffer = new byte[remainingBytes];
+                            streamReader.Read(lastBuffer, 0, remainingBytes);
+                            streamWrite.Write(lastBuffer, 0, lastBuffer.Length);
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
