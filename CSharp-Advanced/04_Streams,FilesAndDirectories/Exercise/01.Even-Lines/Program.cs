c1201dd4f44a665f3d1f4bc39dda57f9d3b1def6
Kristian Ivanov <kriskopk@abv.bg>
2021-03-06 17:58:58 +0200

    Even Lines

---
+++ CSharp-Advanced/04_Streams,FilesAndDirectories/Exercise/01.Even-Lines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+                string pattern = @"[-,.!?]";
+
+
+
+                while (line != null)
+                {
+                    string replaced = Regex.Replace(line, pattern, "@");
+                    if (counter++ % 2 == 0)
+                    {
+
+                        string[] words = replaced.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                        Console.WriteLine(string.Join(" ", words.Reverse()));
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
