1393530e82b50b1d8ca57d591695dab45a356199
Kristian Ivanov <kriskopk@abv.bg>
2021-03-06 17:55:20 +0200

    Odd Lines

---
+++ CSharp-Advanced/04_Streams,FilesAndDirectories/Lab/01.Odd-Lines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _01.Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+
+                while (currentRow != null)
+                {
+                    if (row % 2 == 1)
+                    {
+                        Console.WriteLine(currentRow);
+                    }
+                    currentRow = reader.ReadLine();
+                    row++;
+                }
+            }
+        }
+    }
+}
