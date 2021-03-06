4244420786e9abd7f2be610af0c513496b53da4b
Kristian Ivanov <kriskopk@abv.bg>
2021-03-06 17:55:58 +0200

    Line Numbers

---
+++ CSharp-Advanced/04_Streams,FilesAndDirectories/Lab/02.Line-Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using(StreamReader reader = new StreamReader("input.txt"))
+            {
+                string readRow = reader.ReadLine();
+                int counter = 1;
+
+                using(StreamWriter writer = new StreamWriter("Output.txt"))
+                {
+                    while (readRow!=null)
+                    {
+                        writer.WriteLine($"{counter}. {readRow}");
+                        readRow = reader.ReadLine();
+                        counter++;
+                    }
+                }
+
+            }
+        }
+    }
+}
