404db2f7a38457201065e80df16fd0845d7f4a47
Kristian Ivanov <kriskopk@abv.bg>
2021-03-07 22:15:08 +0200

    Sort Even Numbers

---
+++ CSharp-Advanced/05_Functional Programming/Lab/01.Sort-Even-Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(p => p)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}
