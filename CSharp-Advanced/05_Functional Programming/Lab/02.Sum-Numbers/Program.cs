f97bfe33331e1686372a30d9348aa161fc61e5f6
Kristian Ivanov <kriskopk@abv.bg>
2021-03-07 22:15:35 +0200

    Sum Numbers

---
+++ CSharp-Advanced/05_Functional Programming/Lab/02.Sum-Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = x => int.Parse(x);
+                int[]array = Console.ReadLine().Split(",").Select(parser).ToArray();
+
+            Console.WriteLine(array.Length);
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
