bdfc772de6bed861a9ddb19594969ab892d40c7a
Kristian Ivanov <kriskopk@abv.bg>
2021-03-07 22:11:11 +0200

    Zip and Extract

---
+++ CSharp-Advanced/04_Streams,FilesAndDirectories/Exercise/06.Zip-And-Extract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06.Zip_And_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ZipArchive zipFile = ZipFile.Open("zipFile.zip", ZipArchiveMode.Create);
+            ZipArchiveEntry zipArchuve = zipFile.CreateEntryFromFile("copyMe.png", "newCopyMe.png");
+        }
+    }
+}
