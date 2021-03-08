438b12f7f382680d95ae46dc525fa199818bdc83
Kristian Ivanov <kriskopk@abv.bg>
2021-03-08 22:25:13 +0200

    Knights of honor

---
+++ CSharp-Advanced/05_Functional Programming/Exercise/02.Knights-Of-Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Knights_Of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string> print = x => Console.WriteLine("Sir " + x);
+
+            names.ToList().ForEach(x => print(x));
+        }
+    }
+}
