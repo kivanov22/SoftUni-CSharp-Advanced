66c3e747871155d6fd10dc852f16f94748a60a04
Kristian Ivanov <kriskopk@abv.bg>
2021-03-08 22:24:41 +0200

    Action Point

---
+++ CSharp-Advanced/05_Functional Programming/Exercise/01.Action-Point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string> print = x => Console.WriteLine(x);
+
+            names.ToList().ForEach(x => print(x));
+        }
+
+
+    }
+}
