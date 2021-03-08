1647e7b416e5102019f3cb15491e9743ef8a0bec
Kristian Ivanov <kriskopk@abv.bg>
2021-03-08 22:22:58 +0200

    Add Vat

---
+++ CSharp-Advanced/05_Functional Programming/Lab/04.Add-Vat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Add_Vat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(", ")
+                .Select(Decimal.Parse)
+                .Select(numbers => numbers +(numbers* 0.2m))
+                .ToArray();
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
