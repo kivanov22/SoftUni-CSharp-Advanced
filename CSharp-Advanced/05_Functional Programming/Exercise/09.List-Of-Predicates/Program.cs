2564a0180b2d7c871668e2deda218cb9a076b25b
Kristian Ivanov <kriskopk@abv.bg>
2021-03-09 21:01:57 +0200

    List of Predicates

---
+++ CSharp-Advanced/05_Functional Programming/Exercise/09.List-Of-Predicates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbers = Enumerable.Range(1, end).ToList();
+
+            foreach (int num in numbers)
+            {
+                if (dividers.All(d => num % d == 0))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
