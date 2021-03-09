38794712a70fa6feba9125469866e39df733bdf9
Kristian Ivanov <kriskopk@abv.bg>
2021-03-09 21:03:49 +0200

    Tri Force

---
+++ CSharp-Advanced/05_Functional Programming/Exercise/12.Tri-Function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.Tri_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var targetCharactersSum = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray()
+               .Select(ch => (int)ch).Sum() >= num;
+
+
+            //   names[]   targetCharacter Func <name ,sum,yes or no> theName that matches
+            Func<string[], int, Func<string, int, bool>, string> firstValidName = (arr, num, func) => arr
+                 .FirstOrDefault(str => func(str, num));
+
+            var result = firstValidName(names, targetCharactersSum, isValidWord);
+            Console.WriteLine(result);
+        }
+    }
+}
