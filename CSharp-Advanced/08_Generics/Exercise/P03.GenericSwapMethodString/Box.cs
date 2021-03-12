5c4ff30d9442387210ec9062b399b36a5a6fe2f1
Kristian Ivanov <kriskopk@abv.bg>
2021-03-12 14:02:45 +0200

    Generic Swap Method String

---
+++ CSharp-Advanced/08_Generics/Exercise/P03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.GenericSwapMethodString
+{
+    public class Box<T>
+        
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+    }
+}
---
+++ CSharp-Advanced/08_Generics/Exercise/P03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //List<string> boxes = new List<string>(); valid soltion
+
+            List<Box<int>> boxes = new List<Box<int>>();
+            for (int i = 0; i < n; i++)
+            {
+
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+                boxes.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            SwapIndex(boxes, indexes[0], indexes[1]);
+
+            foreach (Box<int> box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+
+        }
+
+        private static void SwapIndex<T>(List<Box<T>>boxes,int firstIndex,int secondIndex)
+        {
+            Box<T> temp = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+
+    }
+}
