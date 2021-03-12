85ca54e2a48df2783b299f9b92232ce893768a68
Kristian Ivanov <kriskopk@abv.bg>
2021-03-12 13:56:41 +0200

    Generic Box of Strings

---
+++ CSharp-Advanced/08_Generics/Exercise/P01.GenericBoxOfStrings/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.GenericBoxOfStrings
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+    }
+}
---
+++ CSharp-Advanced/08_Generics/Exercise/P01.GenericBoxOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P01.GenericBoxOfStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+                Console.WriteLine(box);
+            }
+            
+        }
+    }
+}
