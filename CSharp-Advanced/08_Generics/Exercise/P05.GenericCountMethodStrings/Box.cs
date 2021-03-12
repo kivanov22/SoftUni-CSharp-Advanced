7ab2bf4da6fd697f11c5347f78a6833dda6b33c0
Kristian Ivanov <kriskopk@abv.bg>
2021-03-12 14:03:25 +0200

    Generic Count Method Strings

---
+++ CSharp-Advanced/08_Generics/Exercise/P05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05.GenericCountMethodStrings
+{
+    public class Box<T>
+        where T: IComparable
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+
+
+            Type valueType = this.Value.GetType();
+            string valueTypeFullname = valueType.FullName;
+
+            return $"{valueTypeFullname}: {this.Value}";
+        }
+    }
+}
