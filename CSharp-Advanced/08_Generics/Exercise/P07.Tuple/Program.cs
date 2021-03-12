253ce7892be15e1a537f60adcf59ae94a168b966
Kristian Ivanov <kriskopk@abv.bg>
2021-03-12 14:03:59 +0200

    Tuple

---
+++ CSharp-Advanced/08_Generics/Exercise/P07.Tuple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P07.Tuple
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstTupleData = Console.ReadLine().Split(" ");
+            string fullName = $"{firstTupleData[0]} {firstTupleData[1]}";
+            CustomTuple<string, string> firstTuple = new CustomTuple<string,string>(fullName, firstTupleData[2]);
+
+            string[] secondTupleData = Console.ReadLine().Split(" ");
+            CustomTuple<string, int> secondTuple = new CustomTuple<string, int>(secondTupleData[0], int.Parse(secondTupleData[1]));
+
+            string[] thirdTupleData = Console.ReadLine().Split(" ");
+            CustomTuple<int, double> thirdTuple = new CustomTuple<int, double>(int.Parse(thirdTupleData[0]), double.Parse(thirdTupleData[1]));
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
