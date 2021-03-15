997e90f14367481fff33086e90d9004693ef661c
Kristian Ivanov <kriskopk@abv.bg>
2021-03-15 10:54:11 +0200

    LootBox

---
+++ CSharp-Advanced/10_ExamPreparation/PartOne/P01.LootBox/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+            Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+
+            int sum = 0;
+            int claimedItems = 0;
+            while (firstBox.Count>0 || secondBox.Count>0)
+            {
+                if (firstBox.Count==0)
+                {
+                    Console.WriteLine("First lootbox is empty");
+                    break;
+                }
+                if (secondBox.Count==0)
+                {
+                    Console.WriteLine("Second lootbox is empty");
+                    break;
+                }
+                int currentFirst = firstBox.Peek();
+                int currentSecond = secondBox.Peek();
+
+                sum = currentFirst + currentSecond;
+                
+                if (sum%2==0)
+                {
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                    claimedItems += sum;
+
+                }
+                else
+                {
+                    firstBox.Enqueue(currentSecond);
+                    secondBox.Pop();
+                }
+            }
+            if (sum>=100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimedItems}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimedItems}");
+            }
+        }
+    }
+}
