bbf95b64fb2580ec12182544d181c9570cd334c6
Kristian Ivanov <kriskopk@abv.bg>
2021-03-19 14:26:04 +0200

    Quick Sort

---
+++ CSharp-Advanced/11_Algorithms/QuickSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = Partition(args, left, right);
+            Sort(a,left,right)
+        }
+        public static void Sort<T>(T[] a) where T : IComparable<T>
+        {
+            Sort(a, 0, a.Length - 1);
+        }
+
+        private static void Sort<T>(T[] a, int left, int right) where T : IComparable<T>
+        {
+            if (left>=right)
+            {
+                return;
+            }
+        }
+
+    }
+}
---
+++ CSharp-Advanced/11_Algorithms/QuickSort/Quick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+    public class Quick
+    {
+
+        //public static void Sort<T>(T[] a)where T :IComparable<T>
+        //{
+
+        //}
+}
+}
