9d544141d5ac88525bb946b49b60c14e69701169
Kristian Ivanov <kriskopk@abv.bg>
2021-03-19 14:25:07 +0200

    Recurive Array Sum

---
+++ CSharp-Advanced/11_Algorithms/P01.RecursiveArraySum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            long sum = Sum(numbers,startIndex:0);
+
+            Console.WriteLine(sum);
+        }
+        public static long Sum(int[] arr ,int startIndex)
+        {
+            if (startIndex==arr.Length-1)
+            {
+                //last element
+                //arr->{4}
+               return arr[startIndex];
+            }
+            return arr[startIndex] + Sum(arr, startIndex: startIndex + 1);
+        }
+    }
+}
