48fecea82bb6d079ae15d7d9e70763344dd4cdf1
Kristian Ivanov <kriskopk@abv.bg>
2021-03-21 17:48:44 +0200

    Restaurant

---
+++ CSharp-OOP/01_Inheritance/Exercise/Restaurant/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var coffee = new Coffee("Costa", 250);
+
+            System.Console.WriteLine(coffee.Price);//maybe 3.50 not 3.5
+        }
+    }
+}
