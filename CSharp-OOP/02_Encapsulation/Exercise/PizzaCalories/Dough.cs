dc4e023f70d89d5f8f7e7d030ab5257f4eaa81c1
Kristian Ivanov <kriskopk@abv.bg>
2021-03-21 18:58:54 +0200

    Pizza Caloriges

---
+++ CSharp-OOP/02_Encapsulation/Exercise/PizzaCalories/Dough.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private const int MinWeight = 1;
+        private const int MaxWeight = 200;
+
+        private string flourType;
+        private string bakingTechnique;
+        private int weight;
+        private const string InvalidDoughExceptionMessage = "Invalid type of dough.";
+
+
+
+        public Dough(string flourType, string bakingTechnique, int weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+        public string FlourType
+        {
+            get => this.flourType;
+            private set
+            {
+                var valueAsLower = value.ToLower();
+                if (valueAsLower != "white" && valueAsLower != "wholegrain")
+                {
+                    throw new ArgumentException(InvalidDoughExceptionMessage);
+                }
+                this.flourType = value;
+            }
+        }
+
+        public string BakingTechnique
+        {
+            get => this.bakingTechnique;
+            set
+            {
+                var valueAsLower = value.ToLower();
+                if (valueAsLower != "chewy" && valueAsLower != "crispy" && valueAsLower != "homemade")
+                {
+                    throw new ArgumentException(InvalidDoughExceptionMessage);
+                }
+                this.bakingTechnique = value;
+
+            }
+        }
+
+        public int Weight
+        {
+            get => this.weight;
+            set
+            {
+                if (value < MinWeight || value > MaxWeight)
+                {
+                    throw new ArgumentException($"Dough weight should be in the range [{MinWeight}..{MaxWeight}].");
+                }
+                this.weight = value;
+            }
+        }
+
+        public double GetCalories()
+        {
+            var flouerTypeModifier = GetFlourTypeModifier();
+            var bakingTechniqueModifier = GetBakingTechniqueModifier();
+            return this.Weight * 2 * flouerTypeModifier * bakingTechniqueModifier;
+        }
+
+        private double GetBakingTechniqueModifier()
+        {
+            var bakingTechniqueLower = this.bakingTechnique.ToLower();
+            if (bakingTechniqueLower == "crispy")
+            {
+                return 0.9;
+            }
+            if (bakingTechniqueLower == "chewy")
+            {
+                return 1.1;
+            }
+            return 1;
+        }
+
+        private double GetFlourTypeModifier()
+        {
+            var flouerTypeLower = this.FlourType.ToLower();
+            if (flouerTypeLower == "white")
+            {
+                return 1.5;
+            }
+            return 1;
+        }
+    }
+}
---
+++ CSharp-OOP/02_Encapsulation/Exercise/PizzaCalories/Topping.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+   public class Topping
+    {
+        private const int min = 1;
+        private const int max = 50;
+        private string name;
+        private int weight;
+
+        public Topping(string name , int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                var valueAsLower = value.ToLower();
+
+                if (valueAsLower!="meat" && valueAsLower!="veggies"&& valueAsLower!="cheese"&& valueAsLower!="sauce")
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+                this.name = value;
+            }
+        }
+        public int Weight
+        {
+            get => this.weight;
+            private set
+            {
+                Validator.ThrowIfNumberIsNotInRange(1, 50, value, $"{this.Name} weight should be in the range [{min}..{max}].");
+                this.weight = value;
+            }
+            
+        }
+        public double GetCalories()
+        {
+            var modifier = GetModifier();
+
+            return this.Weight * 2 * modifier;
+        }
+
+        private double GetModifier()
+        {
+            var nameToLower = this.Name.ToLower();
+
+            if (nameToLower=="meat")
+            {
+                return 1.2;
+
+            }
+            if (nameToLower == "veggies")
+            {
+                return 0.8;
+
+            }
+            if (nameToLower == "cheese")
+            {
+                return 1.1;
+
+            }
+            return 0.9;
+        }
+    }
+}
