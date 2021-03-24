b8d5e6adfc68b1011b4ff1b29ff21214524da1c4
Kristian Ivanov <kriskopk@abv.bg>
2021-03-24 20:18:10 +0200

    PersonInfo

---
+++ CSharp-OOP/03_InterfacesAndAbstraction/Exercise/PersonInfo/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson,IIdentifiable,IBirthable
+    {
+        public Citizen(string name ,int age,string iD,string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = iD;
+            Birthdate = birthdate;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
