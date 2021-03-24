3aa58fea291b824a4ff9c775e6125f6564a69721
Kristian Ivanov <kriskopk@abv.bg>
2021-03-24 20:21:30 +0200

    Shape

---
+++ CSharp-OOP/04_Polymorphism/Lab/Shape/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>()
+                {
+                    new Circle(2.6),
+                    new Circle(12.3),
+                    new Rectangle(4.4, 9.3),
+                    new Rectangle(1.4, 2.7),
+                    
+                };
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine("Shape {0}\nPerimeter = {1}\nArea = {2}\n", shape.GetType().Name, shape.CalculateArea(), shape.CalculatePerimeter());
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/04_Polymorphism/Lab/Shape/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+        public Rectangle(double width,double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (this.Height + this.Width) * 2;
+        }
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
