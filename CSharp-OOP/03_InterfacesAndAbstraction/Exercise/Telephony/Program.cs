1f1bcc75433a5af0c6a7dee5cc9da36aa4dbfe92
Kristian Ivanov <kriskopk@abv.bg>
2021-03-24 20:18:34 +0200

    Telephony

---
+++ CSharp-OOP/03_InterfacesAndAbstraction/Exercise/Telephony/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            string[] urls = Console.ReadLine().Split();
+
+            Smartphone smartphone = new Smartphone();
+            StationaryPhone stationaryPhone = new StationaryPhone();
+
+            foreach (var item in numbers)
+            {
+
+                try
+                {
+                    string result = item.Length == 10 ? smartphone.Call(item) : stationaryPhone.Call(item);
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            foreach (var url in urls)
+            {
+                try
+                {
+                    string result = smartphone.Browse(url);
+                    Console.WriteLine(result);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/03_InterfacesAndAbstraction/Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : Phone,IBrowsable
+    {
+        public string Browse(string url)
+        {
+            if (url.Any(x=>char.IsDigit(x)))
+            {
+                throw new InvalidOperationException("Invalid URL!");
+            }
+            return $"Browsing: {url}!";        }
+
+        public override string Call(string number)
+        {
+            if (number.Any(x=>!char.IsDigit(x)))
+            {
+                throw new InvalidOperationException("Invalid number!");
+            }
+            return $"Calling... {number}";
+        }
+    }
+}
