db906f8e0ccb8a06394e18f4e293a28c785586e6
Kristian Ivanov <kriskopk@abv.bg>
2021-03-24 20:17:24 +0200

    Milityary Elite

---
+++ CSharp-OOP/03_InterfacesAndAbstraction/Exercise/MilitaryElite/Contracts/ILieutenantGeneral.cs
@@ -0,0 +1,15 @@
+﻿using MilitaryElite.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Contracts
+{
+    public interface ILieutenantGeneral:IPrivate
+    {
+        IReadOnlyCollection<IPrivate> Privates { get; }
+
+
+        void AddPrivate(IPrivate @private);
+    }
+}
---
+++ CSharp-OOP/03_InterfacesAndAbstraction/Exercise/MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,28 @@
+﻿using MilitaryElite.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public abstract class Soldier : ISoldier
+    {
+
+        public Soldier(string id , string firstName,string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.FirstName} {this.LastName} Id: {this.Id} ";
+        }
+    }
+}
