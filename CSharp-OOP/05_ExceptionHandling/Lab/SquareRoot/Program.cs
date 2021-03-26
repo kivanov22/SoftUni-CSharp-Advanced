eeaa915937317bddc65b0febad00ca01c56af9a1
Kristian Ivanov <kriskopk@abv.bg>
2021-03-26 20:37:44 +0200

    Square Root

---
+++ CSharp-OOP/05_ExceptionHandling/Lab/SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                int sum = number * number;
+                if (number < 0)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
