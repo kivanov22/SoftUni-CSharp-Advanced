26c2ac99be0fb047d67efe58eba7024e32c95ee8
Kristian Ivanov <kriskopk@abv.bg>
2021-03-26 20:37:20 +0200

    Fixing Vol 2

---
+++ CSharp-OOP/05_ExceptionHandling/Lab/FixingVol2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FixingVol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int num1;
+                int num2;
+                byte result;
+
+                num1 = 10;//30
+                num2 = 5;//60
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+
+        }
+    }
+}
