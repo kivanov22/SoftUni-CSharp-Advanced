f0541591acc2da3d83bb9fac3e17ae9d87723cb8
Kristian Ivanov <kriskopk@abv.bg>
2021-03-27 22:21:38 +0200

    Validation Attributes

---
+++ CSharp-OOP/06_ReflectionAndAttibutes/Exercise/ValidationAttributes/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+   public class Person
+    {
+        private const int minAge = 12;
+        private const int maxAge = 90;
+
+        public Person(string name , int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        [MyRequired]
+        public string Name { get;private set; }
+
+        [MyRange(minAge,maxAge)]//if add false for inclusive age has to be above 12 to give true
+        public int Age { get;private set; }
+    }
+}
---
+++ CSharp-OOP/06_ReflectionAndAttibutes/Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            foreach (var property in properties)    
+            {
+                var attributes = property.GetCustomAttributes().Cast<MyValidationAttribute>().ToArray();
+
+                var value = property.GetValue(obj);
+                foreach (var attribute in attributes)
+                {
+                   bool isValid= attribute.IsValid(value);
+
+                    if (!isValid)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
