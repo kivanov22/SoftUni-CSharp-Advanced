962844cc3ba994e7826c2824e9f6704f3bb8e3e8
Kristian Ivanov <kriskopk@abv.bg>
2021-03-27 22:24:28 +0200

    Skeleton

---
+++ CSharp-OOP/07_UnitTesting/Lab/Skeleton/Contracts/IWeapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton.Tests.Contracts
+{
+    public interface IWeapon
+    {
+        void Attack(ITarget target);
+
+        int AttackPoints { get; }
+
+        int DurabilityPoints { get; }
+
+    }
+}
