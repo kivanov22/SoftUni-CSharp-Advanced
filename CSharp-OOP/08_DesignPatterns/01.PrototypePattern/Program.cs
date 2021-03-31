6b3a188023ebc90c03e29a0adc23d67bec60a13f
Kristian Ivanov <kriskopk@abv.bg>
2021-03-31 15:09:48 +0300

    Prototype Pattern

---
+++ CSharp-OOP/08_DesignPatterns/01.PrototypePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.PrototypePattern
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+
+            sandwichMenu["BLT"] = new Sandwitch("Wheat", "Bacon", "Lettuce", "Tomato");
+            sandwichMenu["PB&J"] = new Sandwitch("Wheat",  "Potato", "Lettuce", "Tomato");
+            sandwichMenu["Turkey"] = new Sandwitch("Swis", "Bacon", "Onion", "Tomato");
+
+
+            Sandwitch sandwich1 = sandwichMenu["BLT"].Clone() as Sandwitch;
+            Sandwitch sandwich2 = sandwichMenu["PB&J"].Clone() as Sandwitch;
+            Sandwitch sandwich3 = sandwichMenu["Turkey"].Clone() as Sandwitch;
+
+
+
+        }
+    }
+}
