d3e4e4d4710124fb0b917bb825335ed705e06596
Kristian Ivanov <kriskopk@abv.bg>
2021-03-31 15:12:08 +0300

    Singleton Pattern

---
+++ CSharp-OOP/08_DesignPatterns/04.Singleton/ISingltonContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Singleton
+{
+    public interface ISingltonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
---
+++ CSharp-OOP/08_DesignPatterns/04.Singleton/SingletonDataContainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _04.Singleton
+{
+    public class SingletonDataContainer : ISingltonContainer
+    {
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+        private Dictionary<string, int> _capitals = new Dictionary<string, int>();
+        
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("capitals.txt");
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                _capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        public static SingletonDataContainer Instance => instance;
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+    }
+}
