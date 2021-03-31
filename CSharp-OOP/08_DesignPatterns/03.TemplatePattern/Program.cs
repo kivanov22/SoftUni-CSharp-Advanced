76f08f5c569e56af84856557cc066e3eb109945e
Kristian Ivanov <kriskopk@abv.bg>
2021-03-31 15:11:35 +0300

    Template Pattern

---
+++ CSharp-OOP/08_DesignPatterns/03.TemplatePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03.TemplatePattern
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+
+        }
+    }
+}
+    //There are easily hundreds of types of bread currently being made in the world,
+    //but each kind involves specific steps in order to make them.
+    //Your task is to model a few different kinds of bread that all use this same pattern,
+    //which is a good fit for the Template Design Pattern.
---
+++ CSharp-OOP/08_DesignPatterns/03.TemplatePattern/WholeWheat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.TemplatePattern
+{
+    public class WholeWheat:Bread
+    {
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Whole Wheats Bread. (15 minutes)");
+        }
+
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering ingredients for Whole Wheat Bread.");
+        }
+    }
+}
