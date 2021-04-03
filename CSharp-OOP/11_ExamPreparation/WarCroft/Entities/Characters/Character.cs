10a6b951c1cfeb7c33750beb45d56c313d4b5113
Kristian Ivanov <kriskopk@abv.bg>
2021-04-03 20:19:15 +0300

    WarCroft

---
+++ CSharp-OOP/11_ExamPreparation/WarCroft/Entities/Characters/Character.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using WarCroft.Constants;
+using WarCroft.Entities.Inventory;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+        private string name;
+        private double health;
+        private double armor;
+
+        // TODO: Implement the rest of the class.
+
+        protected Character(string name ,double health,double armor,double abilityPoints,Bag bag)
+        {
+            Name = name;
+            Health = health;
+            BaseHealth = health;
+            Armor = armor;
+            BaseArmor = armor;
+            AbilityPoints = abilityPoints;
+            Bag = bag;
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.CharacterNameInvalid);
+                }
+                this.name = value;
+            }
+        }
+        public double BaseHealth { get; private set; }
+
+        public double Health
+        {
+            get => this.health;
+            set
+            {
+                if (value<=BaseHealth && value>0)
+                {
+                    this.health = value;
+                }
+            }
+        }
+
+        public double BaseArmor { get; private set; }
+
+        public double Armor
+        {
+            get => this.armor;
+            private set
+            {
+                if (value>0)
+                {
+                    this.armor = value;
+                }
+            }
+        }
+        public double AbilityPoints { get; private set; }
+
+        public IBag Bag { get; private set; }
+        public bool IsAlive { get; set; } = true;
+
+        protected void EnsureAlive()
+        {
+            if (!this.IsAlive)
+            {
+                throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/11_ExamPreparation/WarCroft/Entities/Items/FirePotion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Entities.Characters.Contracts;
+
+namespace WarCroft.Entities.Items
+{
+    public class FirePotion : Item
+    {
+        private const int firePotionWeight = 5; 
+        public FirePotion() 
+            : base(firePotionWeight)
+        {
+        }
+
+        public override void AffectCharacter(Character character)
+        {
+            base.AffectCharacter(character);
+
+
+        }
+    }
+}
